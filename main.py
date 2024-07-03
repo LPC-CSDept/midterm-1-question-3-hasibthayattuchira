de917536b5cf08df15c71f27790e00e841de78b5
hasibthayattuchira <hthayattuchira@gmail.com>
2024-07-03 10:04:40 -0500

    Create main.py

--- main.py
+++ main.py
@@ -3,9 +3,13 @@ def main():
     number = int(input('Enter your number: '))
     pnumber = number + 1
     while True:
-        if number <= 1:
-            return False
-        for i in range (2, int(number ** 0.5) + 1):
+        prime = True
+        for i in range (2, int(pnumber ** 0.5) + 1):
+            if pnumber % i == 0:
+                prime = False
+                break
+        if prime:
+            break
             if number % i == 0:
                 return False
             return True
