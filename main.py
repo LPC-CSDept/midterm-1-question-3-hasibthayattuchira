9b6b98bc532dd2269543d2ef17890cf36e1097e5
hasibthayattuchira <hthayattuchira@gmail.com>
2024-07-03 09:56:07 -0500

    Create main.py

--- main.py
+++ main.py
@@ -1,6 +1,13 @@
 def main():
 
     number = int(input('Enter your number: '))
+    if number <= 1:
+        return False
+    for i in range (2, int(number ** 0.5) + 1):
+        if number % i == 0:
+            return False
+        return True
+    
     """
     ########################################
     Code Your Program here
